1d836e14a2cca3e17781f2827524b91115c36d9c
ihm <s5935512030@phuket.psu.ac.th>
2021-10-09 20:12:06 +0700

    add useInterval in index

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,4 @@
 import useLogic from './useLogic';
 import { useMediaQuery, breakpoints } from './useMediaQuery';
-export { useLogic, useMediaQuery, breakpoints };
+import useInterval from './useInterval';
+export { useLogic, useMediaQuery, breakpoints, useInterval };
