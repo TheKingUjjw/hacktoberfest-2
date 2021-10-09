f43f332185ed4693e72d1a1895bd947da9a4ac02
ihm <s5935512030@phuket.psu.ac.th>
2021-10-09 23:35:39 +0700

    publish useSortRanking and update version

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,12 @@ import useLogic from './useLogic'
 import { useMediaQuery, breakpoints } from './useMediaQuery'
 import useInterval from './useInterval'
 import { useFlatMap } from './useFlatMap'
-export { useLogic, useMediaQuery, breakpoints, useInterval, useFlatMap }
+import useSortRanking from './useSortRanking'
+export {
+  useLogic,
+  useMediaQuery,
+  breakpoints,
+  useInterval,
+  useFlatMap,
+  useSortRanking,
+}
