bc9f908585b62a9977cd5fd6a09d9178d410cf1e
Muhammad A Faishal <maafaishal@gmail.com>
2021-10-17 22:26:23 +0700

    feat: useNavigator

---
+++ src/useNavigator.ts
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react'
+
+type Navigator = {
+  appCodeName: string
+  appName: string
+  appVersion: string
+  cookieEnabled: boolean
+  language: string
+  onLine: boolean
+  platform: string
+  userAgent: string
+}
+
+const useUserAgent = () => {
+  const [navigator, setNavigator] = useState<Navigator>({
+    appCodeName: '',
+    appName: '',
+    appVersion: '',
+    cookieEnabled: false,
+    language: '',
+    onLine: false,
+    platform: '',
+    userAgent: '',
+  })
+
+  useEffect(() => {
+    setNavigator(navigator)
+  }, [])
+
+  return navigator
+}
+
+export default useUserAgent
+©
